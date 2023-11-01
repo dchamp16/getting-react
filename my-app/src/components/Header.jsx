9a37b211dfa23d0af72eb701d22a485b4a3620e1
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2023-11-01 19:45:29 +0000

    added header and style

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,15 +1,32 @@
+import "../component-style/Header.scss";
+
 export default function Header() {
   return (
     <>
-    <header>
-    <ul>
-        <li>Home</li>
-        <li>Portfolio</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </header>
-
+      <header id="head">
+        <ul id="navbar">
+          <li>
+            <a href="" class="home-nav">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="" class="portfolio-nav">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="" class="about-nav">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="" class="contact-nav">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
     </>
   );
 }
