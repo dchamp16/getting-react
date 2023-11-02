58b74dd1f0748d12ce2619784f46c0c547b648a8
Peter Ramos <dchamp16@gmail.com>
2023-11-01 21:48:13 -0600

    added dummy footer and style

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../component-style/Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          posuere erat a ante.
+        </p>
+        <p>Some other footer content...</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
